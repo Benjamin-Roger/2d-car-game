c16d20aa724b887b301855a465c95d076a4aa99c
Benjamin-Roger <llyrian@gmail.com>
2020-12-20 11:02:28 +0100

    Formatting

--- components/SEO.js
+++ components/SEO.js
@@ -4,11 +4,10 @@ import config from "@/config";
 import { useRouter } from "next/router";
 
 export const SEO = ({ title, description }) => {
-
   return (
     <>
       <NextSeo
-        title={title ? title + " | " + config.name  : ''}
+        title={title ? title + " | " + config.name : ""}
         description={description}
         openGraph={{
           title: title,
@@ -20,33 +19,33 @@ export const SEO = ({ title, description }) => {
 };
 
 export const DefaultSEO = () => {
-    const { asPath } = useRouter();
+  const { asPath } = useRouter();
 
-    const baseUrl =  "https://" + (process.env.NEXT_PUBLIC_BASE_URL || config.website);
-  
-    return (
-      <>
-        <DefaultSeo
-          title={config.siteTitle}
-          description={config.defaultSiteDescription}
-          canonical={baseUrl + asPath}
-          openGraph={{
-            type: "website",
-            url: baseUrl  + asPath,
-            title: config.name,
-            description: config.defaultSiteDescription,
-            images: [
-              {
-                url: baseUrl + config.logo.src,
-                width: config.logo.width,
-                height: config.logo.height,
-                alt: config.name,
-              },
-            ],
-            site_name: config.name,
-          }}
-        />
-      </>
-    );
-  };
-  
+  const baseUrl =
+    "https://" + (process.env.NEXT_PUBLIC_BASE_URL || config.website);
+
+  return (
+    <>
+      <DefaultSeo
+        title={config.siteTitle}
+        description={config.defaultSiteDescription}
+        canonical={baseUrl + asPath}
+        openGraph={{
+          type: "website",
+          url: baseUrl + asPath,
+          title: config.name,
+          description: config.defaultSiteDescription,
+          images: [
+            {
+              url: baseUrl + config.logo.src,
+              width: config.logo.width,
+              height: config.logo.height,
+              alt: config.name,
+            },
+          ],
+          site_name: config.name,
+        }}
+      />
+    </>
+  );
+};
