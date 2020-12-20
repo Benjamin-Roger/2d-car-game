308789381d133ccee75cf411f04755c54a7a649e
Benjamin-Roger <llyrian@gmail.com>
2020-12-20 11:07:36 +0100

    Updated About Page

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,11 @@
 export default () => {
   return (
     <>
-      <div className="container mt-30 mx-auto w-full">
-        <h1>About</h1>
-        <h2>Legal notice</h2>
+      <div className="container mt-32 mx-auto w-full">
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <h2  className="text-2xl font-bold mb-3">Legal notice</h2>
         <p>
-          This website is the online version of the gitbup repo located at this
-          address:
+          This website is the online version of the gitbup repo <a href="https://github.com/Benjamin-Roger/2d-car-game" rel="nofollow">https://github.com/Benjamin-Roger/2d-car-game</a>
         </p>
         <p>
           It does not use cookies besides the microphone usage, which requires
@@ -21,7 +20,7 @@ export default () => {
         <p>Walnut, CA 91789</p>
         <p>privacy@vercel.com</p>
 
-        <h2>Credits</h2>
+        <h2  className="text-2xl font-bold mb-3">Credits</h2>
         <a href="https://www.freepik.com/vectors/car">
           Car vector created by pch.vector - www.freepik.com
         </a>
